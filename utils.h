cfdccdf7ad6db050bc6b30f47426e317b91f9d07
Marto-kun <martinvozme456@gmail.com>
2025-11-05 07:13:48 -0500

    Creacion del archivo header

---
+++ utils.h
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+// Funcion de validacion de ingresos de caracteres
+int VerificacionDigitos(char string[])
+{
+
+    if (strlen(string) == 0)
+    {
+        return 0; // Comprobar que la cadena no este vacia
+    }
+
+    for (int i = 0; i < strlen(string); i++)
+    {
+        if (!isdigit(string[i]))
+        {
+            return 0; // Si el elemento de la cadena no es caracter retorna 0
+        }
+    }
+    return 1; // Si la cadena son solo digitos entonces se retorna verdadero
+}
+
+// Funcion para validar un flotante valido
+int VerificacionFloat(char string[])
+{
+    int nPuntos = 0;
+    if (strlen(string) == 0)
+    {
+        return 0; // Comprobar que la cadena no este vacia
+    }
+
+    for (int i = 0; i < strlen(string); i++)
+    {
+        char c = string[i];
+        if (isdigit(c))
+        {
+            continue; // Si el elemento de la cadena es digito continua el bucle
+        }
+        else if (c == '.')
+        {
+            nPuntos++;
+        }
+        else
+        {
+            return 0; // Si no es digito o punto, es invalido
+        }
+    }
+
+    if (nPuntos > 1)
+    {
+        return 0; // No pueden haber mas de 1 punto en el numero decimal
+    }
+
+    if (strlen(string) == 1 && (string[0] == '.'))
+    {
+        return 0; // Cadena invalida si solo es un punto
+    }
+
+    return 1; // Si la cadena son digitos validos entonces se retorna verdadero
+}
+
+// Limpiar buffer
+void LimpiarBuffer()
+{
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF)
+        ;
+}
