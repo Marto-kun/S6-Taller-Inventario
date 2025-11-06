4a7fcb57ae6f6f28114c1826d218956f050049c9
Marto-kun <martinvozme456@gmail.com>
2025-11-05 21:10:25 -0500

    Funcion de promedio y Busqueda de producto añadidas en calculos.h

--- calculos.h
+++ calculos.h
@@ -1,11 +1,15 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
 
-float precioTotal(float precios[])
+/**
+ * @brief Funcion para calcular el precio total del inventario
+ *
+ * @param precios
+ * @param tamano
+ * @return float Precio total de inventario
+ */
+float PrecioTotal(float precios[], int tamano)
 {
-    size_t tamano = sizeof(precios);
     float suma = 0;
     for (int i = 0; i < tamano; i++)
     {
@@ -15,7 +19,18 @@ float precioTotal(float precios[])
     return suma;
 }
 
-void CaroBarato(char productos[], float precios[], int tamano)
+/**
+ * @brief Funcion para encontrar precios caros y baratos
+ *
+ * La funcion va comparando segun una cantidad para ver si el siguiente
+ * elemento es menor o mayor, y segun eso imprime que producto es el mas
+ * caro y barato y su precio
+ *
+ * @param productos
+ * @param precios
+ * @param tamano
+ */
+void CaroBarato(char productos[][25], float precios[], int tamano)
 {
 
     float precioAlto, precioBajo;
@@ -41,4 +56,57 @@ void CaroBarato(char productos[], float precios[], int tamano)
     }
 
     printf("\nEl producto mas caro es %c y cuesta $%.2f", productos[indexAl], precioAlto);
+    printf("\nEl producto mas barato es %c y cuesta $%.2f", productos[indexAl], precioBajo);
+}
+
+/**
+ * @brief Calcular promedio de los precios
+ *
+ * @param precios
+ * @param tamano
+ * @return float promedio
+ */
+
+float Promediar(float precios[], int tamano)
+{
+    float suma = 0, promedio = 0;
+    for (int i = 0; i < tamano; i++)
+    {
+        suma += precios[i];
+    }
+
+    promedio = suma / tamano;
+    return promedio;
+}
+
+/**
+ * @brief Buscador de precios de productos
+ *
+ * La funcion toma el ingreso del usuario y lo compara con la matriz de productos para
+ * determinar cual es el precio asociado.
+ *
+ * @param entrada Ingreso del usuario
+ * @param productos Matriz de nombres de productos
+ * @param precios Vector de precios asociados
+ * @param tamano Numero de filas
+ *  de la matriz de nombres
+ *
+ */
+void BuscarProd(char entrada[], char productos[][25], float precios[], int tamano)
+{
+    int encontrado = 0;
+    for (int i = 0; i < tamano; i++)
+    {
+
+        if (strcmp(entrada, productos[i]) == 0)
+        {
+            printf("\nEl precio del producto %s es: $%.2f", entrada, precios[i]);
+            encontrado = 1;
+            break;
+        }
+    }
+    if (encontrado == 0)
+    {
+        printf("\n Producto no encontrado.");
+    }
 }
